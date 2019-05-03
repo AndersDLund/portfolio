61e64c109f28ae03a58e8aacf78efcb12d0dd5b1
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2019-05-02 22:35:51 -0600

    added animations favicon and changed title

--- src/app/shared/components/certificates/certificates.component.ts
+++ src/app/shared/components/certificates/certificates.component.ts
@@ -7,13 +7,29 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CertificatesComponent implements OnInit {
   @Input() isWeb: boolean;
+  count = 0;
   certificates = [
-    { name: 'Certified Scrum Master', date: 'November 2018', organization: 'Scrum Alliance', img: './assets/images/csm.png' },
-    { name: 'Web Development Immersive', date: 'March 2018', organization: 'Galvanize', img: './assets/images/galvanize.png' },
-    { name: 'Level 1 Sommlier', date: 'March 2017', organization: 'Court of Master Sommeliers', img: './assets/images/somm1.png' }
+    { name: 'Certified Scrum Master', date: 'November 2018', organization: 'Scrum Alliance', img: './assets/images/csm.png',
+     selected: false },
+    { name: 'Web Development Immersive', date: 'March 2018', organization: 'Galvanize', img: './assets/images/galvanize.png',
+     selected: false },
+    { name: 'Level 1 Sommlier', date: 'March 2017', organization: 'Court of Master Sommeliers', img: './assets/images/somm1.png',
+     selected: false }
   ];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    setInterval(() => {
+      for (let i = 0; i < this.certificates.length; i++) {
+        this.certificates[i].selected = false;
+      }
+      if (this.count < 3) {
+        this.certificates[this.count].selected = true;
+        this.count++;
+      } else {
+        this.count = 0;
+      }
+    }, 5000);
+  }
 }
