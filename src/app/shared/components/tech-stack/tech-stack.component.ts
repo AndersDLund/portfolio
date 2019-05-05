4b700b907a173a440c86b126bd57558b7a18e466
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2019-05-05 13:30:05 -0600

    more responsive on xl screens and changed some design

--- src/app/shared/components/tech-stack/tech-stack.component.ts
+++ src/app/shared/components/tech-stack/tech-stack.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechStackComponent implements OnInit {
   tech = [
-    { name: 'Typescript', img: './assets/images/tech/ts.png' },
-    { name: 'Angular', img: './assets/images/tech/angular.png' },
+    { name: 'TYPESCRIPT', img: './assets/images/tech/ts.png' },
+    { name: 'ANGULAR', img: './assets/images/tech/angular.png' },
     { name: 'HTML5', img: './assets/images/tech/html.png' },
     { name: 'SCSS', img: './assets/images/tech/scss.png' }
   ];
