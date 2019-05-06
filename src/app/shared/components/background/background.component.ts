e4cd4f997e6de6628cfffcf6ba5e43f9dd8d68cf
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2019-05-05 18:17:03 -0600

    added and edited some of the text

--- src/app/shared/components/background/background.component.ts
+++ src/app/shared/components/background/background.component.ts
@@ -12,18 +12,18 @@ export class BackgroundComponent implements OnInit {
 
     paragraphs: [
 
-      `While I was working for Audi, we used an app called Record 360 to track vehicle damage.
+      `While I was working for Audi, we used an app called Record 360 to track vehicle damages.
       The app served its purpose but it was essentially just a medium to email pictures and videos.`,
 
-      `I had an idea to improve the app by making it a little more smart using deep
+      `I had an idea to improve the app by making it a little more hands-off by using deep
       learning to reduce the amount of user interaction. I started researching ways to
       bring the app into fruition and found that there were only really two options:
       make the app myself or hire someone to do it for me.`,
 
-      `I found that I could attend a 6 month coding bootcamp
+      `I found that I could attend a six month coding bootcamp
       for less money than it would take to pay someone else to make the app, which is right around when I discovered Galvanize.`,
 
-      `accidental.io (my idea) was my final project at galvanize. I built it in about 2 weeks using a language I didn’t know prior.
+      `Accidental.io (my app) was my final project at galvanize. I built it in about two weeks using a language I didn’t know prior.
       Although, my skills as a developer have improved dramatically since I developed this app and what you see in the video does
       not reflect my current level, it's the project that I'm most proud of. It was the project that made me realize that
       if you can think it, you can build it.`
--- src/app/shared/components/experience/experience.component.ts
+++ src/app/shared/components/experience/experience.component.ts
@@ -14,7 +14,7 @@ export class ExperienceComponent implements OnInit {
       title: 'Full Stack Developer',
       img: './assets/images/software.png',
       // tslint:disable-next-line: max-line-length
-      description: 'After I graduated from Galvanize, I packed my bags and moved to Colorado, a state that I’d never been to before, to work for Blndspt Consulting as a full stack web developer',
+      description: 'After I graduated from Galvanize, I packed my bags and moved to Colorado, a state that I’d never been to before, to work for Blndspt Consulting as a full stack web developer. Here, I continue to refine my skills and learn something new everyday.',
       date: 'June 2018 - Present',
       selected: false,
       hide: false
@@ -24,7 +24,7 @@ export class ExperienceComponent implements OnInit {
       title: 'Brand Specialist',
       img: './assets/images/audi_sketch.png',
       // tslint:disable-next-line: max-line-length
-      description: 'I started working for Audi on Demand as a Brand Specialist to make better use of my degree while exploring a city that I always wanted to spend more time in.',
+      description: 'I started working for Audi on Demand as a Brand Specialist to make better use of my degree while exploring a city that I always wanted to spend more time in. During my time here, I developed a deep interest in mobility and the modern commuter.',
       date: 'April 2017 - October 2017',
       selected: false,
       hide: false
@@ -34,7 +34,7 @@ export class ExperienceComponent implements OnInit {
       title: 'Wine Specialist',
       img: './assets/images/wine1.png',
       // tslint:disable-next-line: max-line-length
-      description: 'My first job out of college was as a Wine Specialist. To prepare for the interview, I put cheap wines under rigorous examinations.',
+      description: 'My first job out of college was as a Wine Specialist. To prepare for the interview, I put cheap wines under rigorous examinations. Near the end of my time working here, I passed the first Sommelier exam and was accepted into the Culinary Institute of America but I decided to switch things up instead.',
       date: 'September 2016 - March 2017',
       selected: false,
       hide: false
